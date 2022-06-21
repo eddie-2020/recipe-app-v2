318c49f14ff57065fd7d7c36f773a2aa90419e07
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-06-21 22:55:28 +0100

    Recipe app - create recipe controller, boilerplate views and request specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: %i[new create edit update destroy]
+  end
+
+  get '/public_recipes', to: 'recipes#public_recipes'
 end
