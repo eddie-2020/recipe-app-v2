60f7e3f9a4bd0a13365615ec6e883c198ed7010d
Thermiee <navyblack16@yahoo.com>
2022-06-22 09:26:42 +0100

    Add validation and food model spec

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,7 @@
 class Food < ApplicationRecord
+  belongs_to :user
+  has_many :ingredients, through: :food_ingredients
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: true
 end
--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :food do
-    
+    name { Faker::Food.dish }
+    measurement_unit { Faker::Food.measurement }
+    price { Faker::Number.decimal(2) }
+    user { FactoryBot.create(:user) }
   end
 end
