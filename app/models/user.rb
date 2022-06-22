b0661cec0b90fde5e4747eec612c9cdf5248f57c
Thermiee <navyblack16@yahoo.com>
2022-06-22 10:18:50 +0100

    made required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :Recipes
+  has_many :Foods
+  validate :name, length: { maximum: 100 }, presence: true
 end
