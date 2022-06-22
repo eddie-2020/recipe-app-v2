815de1a4d5d26e7a4940ae3af9324b219b7c994a
Thermiee <navyblack16@yahoo.com>
2022-06-22 08:50:00 +0100

    Add user migration data

--- db/migrate/20220621191650_create_users.rb
+++ db/migrate/20220621191650_create_users.rb
@@ -1,8 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
-      t.timestamps
+      t.string :name , null: false
+      t.string :email , null: false 
+      t.string :password_digest , null: false 
+      t.string :remember_digest , null: false 
+      t.string :activation_digest , null: false
+      t.boolean :activated , default: false
+      t.datetime :activated_at
+      t.string :reset_digest , null: false
+      t.datetime :reset_sent_at
+      t.timestamps null: false
     end
+
+    add_index :users, :email, unique: true
   end
 end
