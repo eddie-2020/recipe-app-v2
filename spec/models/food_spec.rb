e550ae8a54816dff3c8945f5b092fc07badd241e
Thermiee <navyblack16@yahoo.com>
2022-06-22 09:27:21 +0100

    fix linter error

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Food, type: :model do 
+RSpec.describe Food, type: :model do
   it { should belong_to(:user) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:measurement_unit) }
