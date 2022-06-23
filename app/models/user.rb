31b89a40b308889b4d3e56872acf7e899d5da195
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-06-23 17:00:09 +0100

    Recipe app - fix migration errs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :Recipes
-  has_many :Foods
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :recipes
+  has_many :foods
+
   validate :name, length: { maximum: 100 }, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_200858) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_090735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "recipe_foods", force: :cascade do |t|
-    t.integer "quantity"
-    t.bigint "recipe_id", null: false
+  create_table "foods", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "measurement_unit", null: false
+    t.decimal "price", default: "0.0"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity", default: 1
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
@@ -32,5 +44,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_200858) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
 end
