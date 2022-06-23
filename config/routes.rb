7f860a627da507c48879e50319bcd7a158438621
Thermiee <navyblack16@yahoo.com>
2022-06-23 09:35:12 +0100

    add users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     resources :recipe_foods, only: %i[new create edit update destroy]
   end
 
   get '/public_recipes', to: 'recipes#public_recipes'
+
 end
