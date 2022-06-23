99c36384d3140c16b99bc72ed3e0e470d8e1aa11
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-06-23 10:28:59 +0100

    Recipe app - add recipe helper for total food

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_200858) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_22_121313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "measurement_unit", null: false
+    t.decimal "price", default: "0.0"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_foods_on_name", unique: true
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
@@ -32,5 +43,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_200858) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "foods", "users"
   add_foreign_key "recipe_foods", "recipes"
 end
