76f5c9bf9765cdd7dcf3b5ef0577299533e156b1
Thermiee <navyblack16@yahoo.com>
2022-06-24 07:56:16 +0100

    setup user devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :confirmable
 
   has_many :recipes
   has_many :foods
