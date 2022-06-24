0ebff8fc92bbff43f924d24e83a232ae2ce17e14
Thermiee <navyblack16@yahoo.com>
2022-06-24 12:40:47 +0100

    create shopping list view and controller

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,8 @@
+class ShoppingListsController < ApplicationController
+  include ShoppingListsHelper
+  def index
+    @recipe = Recipe.find(params[:recipe_id])
+    @shopping_list = shoping_list
+    @total_foods_value = total_value_of_food_needed(@shopping_list)
+  end
+end
---
+++ app/helpers/shopping_lists_helper.rb
@@ -0,0 +1,19 @@
+module ShoppingListsHelper
+  def shoping_list
+    @shoppinglist = []
+    @recipe_foods = RecipeFood.where(recipe_id: params[:recipe_id])
+    def total_value_of_food_needed(shoping_list)
+      total_value = 0
+      shoping_list.each do |item|
+        total_value += item[:quantity] * item[:price]
+      end
+      total_value
+    end
+    
+    private
+
+    def food(id)
+      Food.find(id)
+    end
+  end
+end
